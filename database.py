9e43afc14df05e3056e60a6d174e571708165f22
chandan purohit <vyasapurohit@gmail.com>
2021-09-16 15:31:12 +0530

    added feedback analysis

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 import sqlite3
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 def getConnection(dbfile="data.db"):
     try:
@@ -24,6 +25,27 @@ def createTables():
         print("created tables successfully")
     else:
         print("Could not connect to db")
+
+def getCourses():
+    conn = getConnection()
+    courses = conn.execute("select * from course").fetchall()
+    conn.close()
+    return courses
+
+    
+def addFeedback(feedback,course_id):
+    conn = getConnection()
+    sentiment = SentimentIntensityAnalyzer()
+    try:
+        predicted_sentiment = "Positive" if sentiment.polarity_scores(feedback)['compound'] > 0 else "Negative"
+        conn.execute("insert into feedback(id,content,course_id,sentiment) values(?,?,?,?)",(None, feedback, course_id, predicted_sentiment))
+        conn.commit()
+        return f"success! predicted sentiment is {predicted_sentiment}"
+    except Exception as e:
+        print(e)
+        return "failure"
+    finally:
+        conn.close()
 
 def addCourse(name, fees):
     conn = getConnection()
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 import database
 app = Flask(__name__)
 @app.route("/")
@@ -8,19 +8,23 @@ def index():
 @app.route("/course", methods=['GET','POST'])
 def course():
     if request.method == "GET":
-        return render_template("course.html")
+        status = request.args.get("status",0)
+        return render_template("course.html", status=status,courses= database.getCourses())
     else:
         name = request.form['name']
         fees = request.form['fees']
         status = database.addCourse(name, fees)
-        return status
+        return redirect("/course?status="+status)
 
 @app.route("/feedback", methods=['GET', 'POST'])
 def feedback():
     if request.method == "GET":
-        return render_template("feedback.html")
+        status = request.args.get("status",0)
+        print(status)
+        return render_template("feedback.html",status=status, courses=database.getCourses())
     else:
         user_feedback = request.form['feedback']
-        print(user_feedback)
-        return redirect("/feedback")
+        course_id = request.form['course']
+        status = database.addFeedback(user_feedback, course_id)
+        return redirect("/feedback?status="+status)
 app.run(debug=True)
